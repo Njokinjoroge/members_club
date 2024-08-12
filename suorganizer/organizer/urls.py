cdda42b3d0fc22e38330314b6955db370d37ce7b
MorganNjoroge <zeejaymorgan@morgan.com>
2024-08-12 08:44:41 +0300

    changed views

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path ,re_path
 
 from . import views
 
+
 urlpatterns = [
     path('homepage/', views.homepage, name='homepage'),
+    re_path(r'^tag/(?P<slug>[\w\-]+)/$',views.tag_detail, name='organizer_tag_detail'),
 ]
--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
 from .models import Tag
+from django.shortcuts import get_object_or_404
 from django.template import Context, loader
 
+
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context ={'tag_list': tag_list}
+    return render(request,"organizer/tag_list.html",{"tag_list":Tag.objects.all()})
+
+
+
+def tag_detail(request,slug):
+    tag=get_object_or_404(Tag,slug__iexact=slug)
+    template = loader.get_template("organizer/tag_details.html")
+    context ={'tag': tag}
     return HttpResponse(template.render(context))
+
+
+
