2139b35029034d7f88149d51d112c973de77995f
MorganNjoroge <zeejaymorgan@morgan.com>
2024-08-16 07:58:48 +0300

    post detail class view

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,re_path
 
-from . import views
+from .views import Postlist,post_details
 
 urlpatterns = [
-    re_path(r"^$" ,views.post_list,name='blog_post_list'),
-    re_path(r'^(?P<year>\d{4})/' r'(?P<month>\d{1,2})/'r'(?P<slug>[\w\-]+)/$',views.post_details,name='blog_post_details')
+    re_path(r"^$" ,Postlist.as_view(),{'parent_template': 'base.html'},name='blog_post_list'),
+    re_path(r'^(?P<year>\d{4})/' r'(?P<month>\d{1,2})/'r'(?P<slug>[\w\-]+)/$',post_details,{'parent_template': 'base.html'},name='blog_post_details')
 ]
--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
-# Create your views here.
-def post_list(request):
-    return render(request,"post_list.html",{"post_list":Post.objects.all()})
+from django.views.generic import View
+# Create your view
+class Postlist(View):
+    def get(self,request,parent_template=None):
+        return render(request,"post_list.html",{"post_list":Post.objects.all(),'parent_template':parent_template})
 
-def post_details(request,year,month,slug):
+def post_details(request,year,month,slug,parent_template=None):
     post = get_object_or_404(Post,pub_date__year=year,
                              pub_date__month=month,
                              slug=slug)
     
-    return render(request,"post_details.html",{"post":post})
+    return render(request,"post_details.html",{"post":post,'parent_template': parent_template})
