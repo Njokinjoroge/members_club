0764e7f1c2c6bda6ec2c8abb9c9d433a0c9ab8ae
MorganNjoroge <zeejaymorgan@morgan.com>
2024-09-27 01:14:40 +0300

    create tagcreate view

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path ,re_path
 
-from . import views
+from views import tag_detail,tag_list,TagCreate,startup_details,startup_list
 
 
 urlpatterns = [
-    re_path(r"^tag/$",views.tag_list,name="organizer_tag_list"),
-    re_path(r'^tag/(?P<slug>[\w\-]+)/$',views.tag_detail, name='organizer_tag_detail'),
-    re_path(r'^startup/(?P<slug>[\w\-]+)/$',views.startup_details, name='organizer_startup_detail'),
-    re_path(r"^startup/$",views.startup_list,name='organizer_startup_list')
+    re_path(r"^tag/$",tag_list,name="organizer_tag_list"),
+    re_path(r'^tag/create/$',TagCreate.as_view(),name='organizer_tag_create'),
+    re_path(r'^tag/(?P<slug>[\w\-]+)/$',tag_detail, name='organizer_tag_detail'),
+    re_path(r'^startup/(?P<slug>[\w\-]+)/$',startup_details, name='organizer_startup_detail'),
+    re_path(r"^startup/$",startup_list,name='organizer_startup_list')
     
 ]
--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.http.response import HttpResponse
-# Create your views here.
 from .models import Tag,Startup
-from django.shortcuts import get_object_or_404
+from django.views.generic import View
+from django.shortcuts import get_object_or_404,redirect,render
 from django.template import Context, loader
+from.forms import TagForm
 
 
 def tag_list(request):
@@ -25,3 +25,29 @@ def startup_details(request,slug):
     startup=get_object_or_404(Startup,slug__iexact=slug)
     return render(request,"organizer/startup_detail.html",{'startup':startup})
     
+
+# def tag_create(request):
+#     if request.method == 'POST':
+#         form=TagForm(request.POST)
+#         if form.is_valid():
+#             new_tag=form.save()
+#             return redirect(new_tag)
+        
+#     else:
+#         form =TagForm()
+#         return render(request,"organizer/tag_form.html",{'form':form})
+class TagCreate:
+    form_class=TagForm
+    template_name='organizer/tag_form.html'
+    def get(self,request):
+        return render(request,"organizer/tag_form.html",{'form':self.form_class()})
+    def post(self,request):
+        form=self.form_class(request.POST)
+        if form.is_valid():
+            new_tag=form.save()
+            return redirect(new_tag)
+        else:
+            return render(request,self.template_name,{'form':form})
+
+
+        
